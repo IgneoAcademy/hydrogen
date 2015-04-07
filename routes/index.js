1484fd9c3393689b00d0f327a340109cbfe3f283
Rafi Khan <rafi.fa.khan@gmail.com>
2015-04-06 23:31:49 -0400

    initial commit; startup almost done

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET splash page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Weekly view' });
+});
+
+router.get('/welcome', function(req, res, next) {
+  res.render('welcome', { title: 'Welcome!' });
+});
+
+module.exports = router;
